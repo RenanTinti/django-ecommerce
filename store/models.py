e3bcb47f3f55d32c7d60d63869d8d1707c5c48d5
Renan Tinti <renantinti@gmail.com>
2022-09-18 13:12:00 -0300

    Dynamic total value and total items in cart

--- store/models.py
+++ store/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    # This decorator avoids error when there is no URL loaded in template
+    # This decorator method (as property) avoids error when there is no URL loaded in template
     @property
     def image_url(self):
         try:
@@ -38,13 +38,33 @@ class Order(models.Model):
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return (self.id)
+        return str(self.id)
+
+    # This decorator returns the total value of items in cart, based on OrderItem.get_total() method
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    # This decorator returns the total quantity of items in cart, based on OrderItem.quantity property
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    # This decorator return the total value of a single type of item added in cart (for example: if I'm buying 2 shirts and a 3 watches, this method will return the total value of 2 shirts and total value of 3 watches separately, and not the sum of the total values)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
--- store/views.py
+++ store/views.py
@@ -9,12 +9,41 @@ def store(request):
     context = {
         'products': products,
     }
+
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+        # Query: get all the child order items that have the order as a father objects. Must be written in lower case and _set.all() right after
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {
+        'items': items,
+        'order': order,
+    }
+
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {
+        'items': items,
+        'order': order,
+    }
+
     return render(request, 'store/checkout.html', context)
